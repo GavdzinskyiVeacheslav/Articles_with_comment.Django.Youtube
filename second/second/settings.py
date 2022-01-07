20ed9143fa06cb5ff2761e7d4ef1d6146178895c
Gavdzinskyi <veacheslavgavdzynsky@gmail.com>
2022-01-07 21:26:47 +0200

    Removed the secret key

--- second/second/settings.py
+++ second/second/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i7+_6)xc*9tibxwn3)9=1vyxgba0hso%y@cwc&w*v(!qd_&3ib'
+SECRET_KEY = '---'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
